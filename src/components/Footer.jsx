8856893f48b36c3a727f45c7437a2e6ec735a66a
piiyuushh <piyushkarn76@gmail.com>
2024-10-29 19:51:38 +0545

    footer update handiyo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,37 +3,11 @@ import { FaGithub } from "react-icons/fa"; // Import GitHub icon
 
 const Footer = () => {
   return (
-    <footer className="py-6 bg-[#FAFAFA] border-t border-[#EBEBEB]  text-gray-800 dark:bg-[#0A0A0A] dark:border-[#252525] dark:text-white">
-      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center px-4">
-        {/* Navigation Links */}
-        <div className="flex flex-wrap justify-center md:justify-start space-x-4 mt-4 md:mt-0">
-          <Link to="/" className="hover:text-blue-600 dark:hover:text-blue-400">
-            Home
-          </Link>
-          <Link to="/components" className="hover:text-blue-600 dark:hover:text-blue-400">
-            Components
-          </Link>
-          <Link to="/docs" className="hover:text-blue-600 dark:hover:text-blue-400">
-            Docs
-          </Link>
-        </div>
+    <footer className=" bg-[#FAFAFA] border-t border-[#EBEBEB]  text-gray-800 dark:bg-[#0A0A0A] dark:border-[#252525] dark:text-white">
+      
 
-        {/* GitHub Link */}
-        <div className="mt-4 md:mt-0">
-          <a
-            href="https://github.com/smrn001/Sajilo-UI"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center hover:text-blue-600 dark:hover:text-blue-400"
-          >
-            <FaGithub className="mr-1" /> {/* GitHub Icon */}
-            GitHub
-          </a>
-        </div>
-      </div>
-
-      <div className="text-center mt-4">
-        <p className="text-sm">
+      <div className="text-center py-5">
+        <p className="text-base">
           © {new Date().getFullYear()} Sajilo UI. All rights reserved.
         </p>
       </div>
