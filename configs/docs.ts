055203c3978306b48da4d08c6c2b7f31db6cfb26
smrn001 <samir.niroula001@gmail.com>
2024-10-31 18:57:40 +0545

    sorted components

--- configs/docs.ts
+++ configs/docs.ts
@@ -1,35 +1,12 @@
 export const SpecialComponents = [
+  { href: '/components/bentogrid', name: 'Bento Grid ' },
   { href: '/components/buttons', name: 'Buttons' },
-
-  {
-    href: '/components/card',
-    name: 'Cards ',
-    new: true,
-  },
-
+  { href: '/components/card', name: 'Cards ', new: true },
+  { href: '/components/demo', name: 'Demo ' },
   { href: '/components/footers', name: 'Footers' },
-  {
-    href: '/components/demo',
-    name: 'Demo ',
-  },
-  {
-    href: '/components/bentogrid',
-    name: 'Bento Grid ',
-  },
-  {
-    href: '/components/hero',
-    name: 'Hero Section',
-  },
-  {
-    href: '/components/form',
-    name: 'Form ',
-  },
-
-  {
-    href: '/components/navbar',
-    name: 'Navbar ',
-    new: true,
-  },
+  { href: '/components/form', name: 'Form ' },
+  { href: '/components/hero', name: 'Hero Section' },
+  { href: '/components/navbar', name: 'Navbar ', new: true },
 ];
 
 // export const MainComponents = [
