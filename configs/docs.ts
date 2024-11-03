1c7f2acb68bf926b0769e75453ae39caaa316f91
smrn001 <samir.niroula001@gmail.com>
2024-11-03 20:44:49 +0545

    removed marquee as it was laggy

--- configs/docs.ts
+++ configs/docs.ts
@@ -1,22 +1,22 @@
 export const SpecialComponents = [
   { href: '/components/bentogrid', name: 'Bento Grid' },
   { href: '/components/bottom', name: 'Bottom Navbar', new: true },
-  { href: '/components/breadcrumb', name: 'Breadcrumb', new: true },
+  { href: '/components/breadcrumb', name: 'Breadcrumb' },
   { href: '/components/card', name: 'Cards' },
   { href: '/components/carousel', name: 'Carousel' },
   { href: '/components/demo', name: 'Demo' },
-  { href: '/components/drawer', name: 'Drawer', new: true },
+  { href: '/components/drawer', name: 'Drawer' },
   { href: '/components/error', name: 'Error' },
   { href: '/components/footers', name: 'Footers' },
   { href: '/components/form', name: 'Form' },
   { href: '/components/hero', name: 'Hero Section' },
   { href: '/components/marquee', name: 'marquee', new: true },
-  { href: '/components/modal', name: 'Modal', new: true },
+  { href: '/components/modal', name: 'Modal' },
   { href: '/components/navbar', name: 'Navbar' },
   { href: '/components/parallax', name: 'Parallax' },
   { href: '/components/price', name: 'Price' },
   { href: '/components/table', name: 'Table' },
-  { href: '/components/notification', name: 'Toast ', new: true },
+  { href: '/components/notification', name: 'Toast '},
 ];
 // export const MainComponents = [
 //   {
