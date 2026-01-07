68d9d5434aef09f96044c10606d705bc960d27ec
“piiiyuushh” <piyushkarn76@gmail.com>
2026-01-07 23:12:39 +0545

    feat: added 3d UI temporarily

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,15 @@ import Header from '@/components/website/header';
 import HeroSec from '@/components/website/hero-sec';
 import Footer from '@/components/website/footer';
 import SnowfallEffect from '@/components/website/snowfall-effect';
+// import AnimatedShaderBackground from '@/components/ui/animated-shader-background';
 
 
 export default function Home() {
   return (
     <>
-      <Header />
       <SnowfallEffect />
+      {/* <AnimatedShaderBackground /> */}
+      <Header />
       <main className='relative'>
         <HeroSec />
       </main>
